6e323a638999dd09e7a6b0d16f2998d508aabbba
Cosmo Grant <cosmodgrant@gmail.com>
2025-11-20 22:07:54 +0000

    add script to generate index.html

---
+++ scripts/make_index.py
@@ -0,0 +1,36 @@
+import subprocess
+from pathlib import Path
+
+from jinja2 import Environment, FileSystemLoader
+
+
+def get_source_folder():
+    "Get a path to the folder that github pages publishes from."
+    proc = subprocess.run(["git", "rev-parse", "--show-toplevel"], check=True, capture_output=True)
+    repo_root = proc.stdout.decode("utf-8").strip()
+    repo_root = Path(repo_root)
+    source_folder = repo_root / "docs"
+
+    return source_folder
+
+
+def get_display_name(file):
+    return file.stem.replace("_", " ")
+
+
+def main():
+    env = Environment(loader=FileSystemLoader("templates/"))
+    index_template = env.get_template("index.jinja")
+
+    source_folder = get_source_folder()
+
+    files = [(str(file), get_display_name(file)) for file in source_folder.iterdir() if file.name != "index.html"]
+    files.sort(key=lambda file: file.stat().st_ctime, reverse=True)  # newest created first
+
+    rendered = index_template.render(files=files)
+    with open(source_folder / "index.html", "w") as f:
+        f.write(rendered)
+
+
+if __name__ == "__main__":
+    main()
